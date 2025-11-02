875d9e87eb1c0bf4b3a1f03a0663ff858effebd7
s3chkin <seckins191@gmail.com>
2025-11-02 19:01:43 +0200

    Add new win app project

---
+++ Salary/Salary/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace Salary
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            listBox1 = new ListBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(213, 12);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(191, 23);
+            textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(213, 49);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(191, 23);
+            textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(215, 86);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(191, 23);
+            textBox3.TabIndex = 2;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(215, 124);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(191, 23);
+            textBox4.TabIndex = 3;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(47, 190);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 4;
+            button1.Text = "Зареди";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(47, 236);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 5;
+            button2.Text = "Добави";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(47, 286);
+            button3.Name = "button3";
+            button3.Size = new Size(75, 23);
+            button3.TabIndex = 6;
+            button3.Text = "Изтрий";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(47, 336);
+            button4.Name = "button4";
+            button4.Size = new Size(75, 23);
+            button4.TabIndex = 7;
+            button4.Text = "Запиши";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 15;
+            listBox1.Location = new Point(184, 190);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(263, 169);
+            listBox1.TabIndex = 8;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(19, 20);
+            label1.Name = "label1";
+            label1.Size = new Size(29, 15);
+            label1.TabIndex = 9;
+            label1.Text = "име";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(19, 57);
+            label2.Name = "label2";
+            label2.Size = new Size(51, 15);
+            label2.TabIndex = 10;
+            label2.Text = "Заплата";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(19, 94);
+            label3.Name = "label3";
+            label3.Size = new Size(43, 15);
+            label3.TabIndex = 11;
+            label3.Text = "Месец";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(19, 132);
+            label4.Name = "label4";
+            label4.Size = new Size(46, 15);
+            label4.TabIndex = 12;
+            label4.Text = "Година";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(471, 377);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(listBox1);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private ListBox listBox1;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+    }
+}
---
+++ Salary/Salary/Form1.cs
@@ -0,0 +1,91 @@
+﻿namespace Salary
+{
+    public partial class Form1 : Form
+    {
+        string filePath = "C:\\Users\\Sechkin\\Documents\\TSP\\Salary\\Salary\\rabotnici.txt";
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            LoadDataFromFile();
+        }
+
+        private void LoadDataFromFile()
+        {
+            listBox1.Items.Clear();
+
+            if (File.Exists(filePath))
+            {
+                string[] lines = File.ReadAllLines(filePath);
+                foreach (string line in lines)
+                {
+                    listBox1.Items.Add(line);
+                }
+            }
+        }
+
+        private void SaveDataToFile()
+        {
+            using (StreamWriter writer = new StreamWriter(filePath))
+            {
+                foreach (var item in listBox1.Items)
+                {
+                    writer.WriteLine(item.ToString());
+                }
+            }
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            LoadDataFromFile();
+            MessageBox.Show("Данните са заредени успешно!");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string name = textBox1.Text.Trim();
+            string salary = textBox2.Text.Trim();
+            string month = textBox3.Text.Trim();
+            string year = textBox4.Text.Trim();
+
+            if (name == "" || salary == "" || month == "" || year == "")
+            {
+                MessageBox.Show("Моля, попълнете всички полета!");
+                return;
+            }
+
+            string record = $"{name}, {salary}, {month}, {year}";
+            listBox1.Items.Add(record);
+
+            SaveDataToFile();
+
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex != -1)
+            {
+                listBox1.Items.RemoveAt(listBox1.SelectedIndex);
+                SaveDataToFile();
+            }
+            else
+            {
+                MessageBox.Show("Моля, изберете запис за изтриване!");
+            }
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            SaveDataToFile();
+            MessageBox.Show("Данните са записани успешно!");
+
+        }
+    }
+}
